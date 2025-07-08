edb3d115e7b4ce640d036b7408414b4871278dff
Rndns <bible1218@naver.com>
2025-07-08 20:34:20 +0900

    [add] home text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,53 @@
 const Home = () => {
-  return <div>Home</div>;
+
+  return (
+  <div>
+    <div><p>toss</p><button>이미지</button></div>
+    <div>
+      <div>토스뱅크</div>
+      <div>1,234,560원</div>
+      <div>
+        <button>송금</button>
+        <button>충전</button>
+        <button>ATM</button>
+      </div>
+    </div>
+    <div>
+      <div>빠른 서비스</div>
+      <div>
+        <button>
+          <div>이미지</div>
+          <p>QR결제</p>
+          <p>스캔하고 간편결제</p>
+        </button>
+        <button>
+          <div>이미지</div>
+          <p>바코드결제</p>
+          <p>매장에서 바코드 제시</p>
+        </button>
+        <button>
+          <div>이미지</div>
+          <p>온라인결제</p>
+          <p>웹/앱에서 간편결제</p>
+        </button>
+      </div>
+    </div>
+    <div>
+      <div>
+        <div>최근거래</div>
+        <div>전체</div>
+      </div>
+      <div>
+        <div>이미지</div>
+        <div>
+          <div>스타벅스 강남점</div>
+          <div>12:34</div>
+        </div>
+        <div>-4,500원</div>
+      </div>
+    </div>
+  </div>
+  )
 };
 
 export default Home;
