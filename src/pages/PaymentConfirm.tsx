7420aba06c87d5ad98711f1030f1206ad030b0fb
wonil <nwi6959@gmail.com>
2025-07-10 10:27:34 +0900

    fix: 리뷰어 자동 지정 오류 수정

--- src/pages/PaymentConfirm.tsx
+++ src/pages/PaymentConfirm.tsx
@@ -1,16 +1,5 @@
-import Header from '@/components/Header';
-import cancelIcon from '@/assets/cancel.svg';
-
-const cancel = () => {
-  return <img src={cancelIcon} alt="" />;
-};
-
 const PaymentConfirm = () => {
-  return (
-    <div>
-      <Header prevBtn title="결제하기" right={cancel()} />
-    </div>
-  );
+  return <div>PaymentConfirm</div>;
 };
 
 export default PaymentConfirm;
