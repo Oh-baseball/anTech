4edae8213957e31c24e5c0a135c2195c4290679b
dabin <dabin24681379@gmail.com>
2025-07-12 11:04:11 +0900

    feat: 결제수단 css 작업

--- src/features/paymentMethod/PaymentMethodBox/index.tsx
+++ src/features/paymentMethod/PaymentMethodBox/index.tsx
@@ -1,25 +1,26 @@
-import toss_icon from './assets/토스 아이콘.svg'
-import after_select from './assets/체크 박스.svg'
-import before_select from './assets/선택전 아이콘.svg'
+import toss_icon from '@/assets/tossicon.svg'
+import after_select from '@/assets/selection.svg'
+import before_select from '@/assets/unselection.svg'
+
 import styles from './style.module.scss';
+// import PaymentMethodBox from "@/features/paymentMethod/Components/PaymentMethodBox";
 
 const PaymentMethodBox = () => {
     return(
-        // <div className='payment_method_container'>
-        <div className={styles.PaymentMethodBox}>
+        <div className={styles.payment_method_container}>
             <div>
                 <img src={toss_icon} alt="토스 아이콘"/>
-                <div className='payment_method_info'>
+                <div className={styles.payment_method_info}>
                     <p>토스페이</p>
                     <p>잔액 1,234,560원</p>
                 </div>
             </div>
-            <div className='check'>
-                <div className='selected'>
+            <div className={styles.check}>
+                <div className={styles.selected}>
                     <img src={after_select} alt="선택 후 아이콘"/>
                     <p>선택됨</p>
                 </div>
-                <div className='unselected'>
+                <div className='unselected' style={{display:'none'}}>
                     <img className='unselected' src={before_select} alt="선택 전 아이콘" />
                 </div>
             </div>
--- src/pages/PaymentMethod.tsx
+++ src/pages/PaymentMethod.tsx
@@ -1,5 +1,31 @@
+import PaymentMethodBox from "@/features/paymentMethod/PaymentMethodBox";
+
 const PaymentMethod = () => {
-  return <div>PaymentMethod</div>;
+  return <div className='payment_method'>
+            <div className='payment_info'>
+                <div className='store_info'>
+                    {/* <img src={store_icon} alt="" /> */}
+                    {/* <img src="https://url.kr/fb9r49" alt="스타벅스 로고" /> */}
+                    <span>스타벅스 강남점</span>
+                </div>
+                <div className='payment_amount'>
+                    <p>총 결제금액</p>
+                    <p style={{color:'#FAFCFF', fontSize:'20px', fontWeight:700}}>9,500원</p>
+                </div>
+            </div>
+            
+            <h3>간편결제</h3>
+            <PaymentMethodBox />
+
+            <h3>카드.계좌</h3>
+            <PaymentMethodBox />
+            <PaymentMethodBox />
+            
+            <h3>기타</h3>
+            <PaymentMethodBox />
+            
+            {/* <Button /> */}
+        </div>
 };
 
 export default PaymentMethod;
