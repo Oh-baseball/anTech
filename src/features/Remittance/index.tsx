9abdb82f59c2e981537ac90d0d6a46c94bfb42b0
Rndns <bible1218@naver.com>
2025-07-22 19:56:43 +0900

    feat: rotate coin

--- src/features/Remittance/index.tsx
+++ src/features/Remittance/index.tsx
@@ -2,18 +2,12 @@ import styles from './style.module.scss';
 
 const RemittanceLayout = () => {
   return (
-    <div className={styles.coinarea}>
-      <div className={`${styles.coin} ${styles.coin1}`}></div>
-      <div className={`${styles.coin} ${styles.coin2}`}></div>
-      <div className={`${styles.coin} ${styles.coin3}`}></div>
-      <div className={`${styles.coin} ${styles.coin4}`}></div>
-      <div className={`${styles.coin} ${styles.coin5}`}></div>
-      <div className={`${styles.coin} ${styles.coin6}`}></div>
-      <div className={`${styles.coin} ${styles.coin7}`}></div>
-      <div className={`${styles.coin} ${styles.coin8}`}></div>
-      <div className={`${styles.coin} ${styles.coin9}`}></div>
+    <div className={styles.coin}>
+      <div className={styles.cylinder_edge}>
+        <div className={styles.cylinder_top}></div>
+        <div className={styles.cylinder_bottom}></div>
+      </div>
     </div>
-
   )
 };
 
