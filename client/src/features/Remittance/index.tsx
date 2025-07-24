216f484b043fd5be2c423e27d18a3918692c1fd7
Rndns <bible1218@naver.com>
2025-07-24 21:49:49 +0900

    feat: transfer money path5-spin

--- client/src/features/Remittance/index.tsx
+++ client/src/features/Remittance/index.tsx
@@ -1,8 +1,9 @@
 import Header from '@/components/Header';
 import AccountBox, { AccountBoxProps } from './AccountBox';
 import styles from './style.module.scss';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { CreditCard } from 'lucide-react';
+import Coin from '@/components/Coin';
 
 interface RemittanceLayoutProps {
   userAccount: AccountBoxProps;
@@ -19,6 +20,8 @@ const RemittanceLayout = ({userAccount}:RemittanceLayoutProps) => {
   const [accountNumber, setAccountNumber] = useState('input');
   const [memo, setMemo] = useState('input');
   const [checkTransfer, setCheckTransfer] = useState(false);
+  const [checkCoin, setCheckCoin] = useState(false);
+  const coin1 = useRef<HTMLDivElement>(null);
 
   const handleCancel = () => {
     console.log('취소 버튼 클릭');
@@ -26,7 +29,10 @@ const RemittanceLayout = ({userAccount}:RemittanceLayoutProps) => {
 
   const handletransfer = () => {
     setCheckTransfer(true);
-
+    setCheckCoin(true);
+    if (coin1.current) {
+      coin1.current.style.transform = "rotate(30deg)";
+    }
   };
 
   return (
@@ -35,12 +41,40 @@ const RemittanceLayout = ({userAccount}:RemittanceLayoutProps) => {
         prevBtn={true}
         title="입금"
       />
-      <div className={styles.coinBox}>
-        <div className={styles.coin1}></div>
-        <div className={styles.coin2}></div>
-        <div className={styles.coin3}></div>
-        <div className={styles.coin4}></div>
-        <div className={styles.coin5}></div>
+
+      <div className={`${styles.coin_container} ${checkCoin ? styles.coin_check : ''}`}>
+        {/* {Array.from({ length: 5}).map((_, i) => (
+          <div
+            key={`coinNev${i}`}
+            className={`${styles['coin' + i + 'Outside']}`}
+            ref={}
+          ></div>
+        ))} */}
+        <div className={styles.coin1Outside}>
+          <div className={styles.coin1Inside} ref={coin1}>
+            <Coin scale={0.2}/>
+          </div>
+        </div>
+        <div className={styles.coin2Outside}>
+          <div className={styles.coin2Inside}>
+            <Coin scale={0.2}/>
+          </div>
+        </div>
+        <div className={styles.coin3Outside}>
+          <div className={styles.coin3Inside}>
+            <Coin scale={0.2}/>
+          </div>
+        </div>
+        <div className={styles.coin4Outside}>
+          <div className={styles.coin4Inside}>
+            <Coin scale={0.2}/>
+          </div>
+        </div>
+        <div className={styles.coin5Outside}>
+          <div className={styles.coin5Inside}>
+            <Coin scale={0.2}/>
+          </div>
+        </div>
       </div>
 
       <div className={styles.accountBox}>
