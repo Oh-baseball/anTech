6e38ff7ad62c4c47293ce6dfab0702652dd04285
wonil <nwi6959@gmail.com>
2025-07-24 19:56:11 +0900

    chore: .env git ignore에 추가 및 axiosInstance 설정

---
+++ client/src/apis/axiosInstance.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosInstance } from 'axios';
+
+export const axiosInstance: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_SERVER_URL,
+  timeout: 5000,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401) {
+      // 인증 에러 처리
+      //todo refresh 요청
+    }
+    return Promise.reject(error);
+  },
+);
