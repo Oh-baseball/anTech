587613d547082c30eaf47a7f291f00e0bfa7b905
wonil <nwi6959@gmail.com>
2025-07-26 09:34:47 +0900

    chore: 불필요한 주석 제거

--- client/src/apis/axiosInstance.ts
+++ client/src/apis/axiosInstance.ts
@@ -8,26 +8,3 @@ export const axiosInstance: AxiosInstance = axios.create({
     'Content-Type': 'application/json',
   },
 });
-
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem('token');
-//     if (token) {
-//       config.headers['Authorization'] = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   },
-// );
-
-// axiosInstance.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response.status === 401) {
-//       console.error(error);
-//     }
-//     return Promise.reject(error);
-//   },
-// );
