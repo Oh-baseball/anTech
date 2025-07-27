2192930dc3f70afb73728e130ccded2cc2656162
wonil <nwi6959@gmail.com>
2025-07-28 01:08:41 +0900

    feat: 리액트 쿼리 세팅 및 유저, 결제내역 훅 추가

--- client/src/types/api.ts
+++ client/src/types/api.ts
@@ -1,3 +1,5 @@
+import { QueryKey, UseMutationOptions, UseQueryOptions } from '@tanstack/react-query';
+
 export type APIResponse<T> = {
   success: boolean;
   message: string;
@@ -14,3 +16,13 @@ export interface ErrorResponse {
   method: HttpMethod;
   message: string;
 }
+
+export type UseQueryCustomOptions<TQueryFnData = unknown, TData = TQueryFnData> = Omit<
+  UseQueryOptions<TQueryFnData, APIResponse<ErrorResponse>, TData, QueryKey>,
+  'queryKey'
+>;
+
+export type UseMutationCustomOptions<TData = unknown, TVariables = void> = Omit<
+  UseMutationOptions<TData, APIResponse<ErrorResponse>, TVariables, unknown>,
+  'mutationFn'
+>;
