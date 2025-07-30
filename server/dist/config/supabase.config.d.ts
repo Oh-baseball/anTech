0ecc44dabec66c8007ebca349fe98d50d73bec40
JongHwan <86533373+MSbtff@users.noreply.github.com>
2025-07-30 20:01:36 +0900

    Delete server directory

--- server/dist/config/supabase.config.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class SupabaseConfig {
-    private supabase;
-    constructor();
-    getClient(): any;
-    testConnection(): Promise<boolean>;
-}
-export declare const createSupabaseClient: () => import("@supabase/supabase-js").SupabaseClient<any, "public", any>;
